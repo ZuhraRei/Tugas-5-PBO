a0e398a718645fd126d3608e78346b83c2ed5c2c
ZuhraRei <92098479+ZuhraRei@users.noreply.github.com>
2021-10-12 09:51:55 +0700

    Add files via upload

---
+++ Polymorphism/Karyawan.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Polymorphism
+{
+    public abstract class Karyawan
+    {
+        public string NamaDepan { get; }
+        public string NamaBelakang { get; }
+        public string NomorKTP { get; }
+        public Karyawan(string namaDepan, string namaBelakang, string nomorKTP)
+        {
+            NamaDepan = namaDepan;
+            NamaBelakang = namaBelakang;
+            NomorKTP = nomorKTP;
+        }
+        public override string ToString() => $"{NamaDepan} {NamaBelakang}\n" + $"Nomor KTP: {NomorKTP}";
+        public abstract decimal Pendapatan();
+    }
+}
