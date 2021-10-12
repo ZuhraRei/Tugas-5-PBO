4422b6238b0ea8b4fa5538252fb96459e01a6342
ZuhraRei <92098479+ZuhraRei@users.noreply.github.com>
2021-10-12 09:54:25 +0700

    Add files via upload

---
+++ Polymorphism/ClassGaji.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Polymorphism
+{
+    public class KaryawanGaji : Karyawan
+    {
+        private decimal gajiMingguan;
+        public KaryawanGaji(string namaDepan, string namaBelakang, string nomorKTP, decimal gajiMingguan) : base(namaDepan, namaBelakang, nomorKTP)
+        {
+            GajiMingguan = gajiMingguan;
+        }
+
+        public decimal GajiMingguan
+        {
+            get
+            {
+                return gajiMingguan;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentOutOfRangeException(nameof(value), value, $"{nameof(GajiMingguan)} harus >= 0");
+                }
+                gajiMingguan = value;
+            }
+        }
+
+        public override decimal Pendapatan() => GajiMingguan;
+        public override string ToString() => $"karyawan bergaji: {base.ToString()}\n" + $"gaji mingguan: {GajiMingguan:C}";
+    }
+
+}
